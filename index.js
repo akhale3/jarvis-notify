9685c010b9b5a605d731ca7db406d002160cb4f3
akhale3 <anish@firstperformance.com>
2016-11-05 20:32:38 -0400

    Fix route callback function signature

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
+#!/usr/bin/env node
+'use strict';
+
+var path = require('path');
+var mail = require(path.resolve(__dirname, '.', 'lib', 'mail'));
+var Promise = require('bluebird'); // eslint-disable-line
+
 exports.route = {
   httpMethod: 'POST',
   path: 'notify',  // No leading slash
   callback: function callback(req, res, next) {
-    res.send('Notification dictionary ' + req.params.dictionary);
-    next();
+    if (!req.params.channel || req.params.channel.indexOf('email') > -1) {
+      mail.sendMail(req.params)
+      .then(function respond(info) {
+        res.send(info);
+        next();
+      })
+      .catch(function sendMailCatch(error) { // eslint-disable-line
+        // TODO: Log error
+      });
+    } else if (req.params.channel.indexOf('slack') > -1) {
+      // TODO: Slack API integration
+    }
   }
 };
