0ce87ca2ba269fa7f3431e476074961beaa9440f
akhale3 <anish@firstperformance.com>
2016-11-16 16:28:40 -0500

    Promisify and generalize

--- index.js
+++ index.js
@@ -2,40 +2,38 @@
 'use strict';
 
 var path = require('path');
-var mail = require(path.resolve(__dirname, '.', 'lib', 'mail'));
-var slack = require(path.resolve(__dirname, '.', 'lib', 'slack'));
-var Promise = require('bluebird'); // eslint-disable-line
+var Promise = require('bluebird');
 
 exports.route = {
   httpMethod: 'POST',
   path: 'notify',  // No leading slash
   callback: function callback(req, res, next) {
-    if (!req.params.channel || req.params.channel.indexOf('email') > -1) {
+    var channels = [];
 
-      // Email integration
-      mail.sendMail(req.params)
-      .then(function respond(info) {
-        res.send(info);
-        next();
-      })
-      .catch(function sendMailCatch(error) { // eslint-disable-line
-        // TODO: Log error
-        res.send(error);
-        next();
-      });
-    } else if (req.params.channel.indexOf('slack') > -1) {
-
-      // Slack API integration
-      slack.sendSlackMessage(req.params)
-      .then(function respond(info) {
-        res.send(info);
-        next();
-      })
-      .catch(function sendSlackMessageCatch(error) { // eslint-disable-line
-        // TODO: Log error
-        res.send(error);
-        next();
-      });
+    if (req.params.channel) {
+      channels = req.params.channel.split(/[\s,]+/);
+    } else {
+      channels.push('email');
     }
+
+    Promise.all(channels.map(function mapCallback(channel) {
+      var handler = require(path.resolve(__dirname, '.', 'lib', channel));
+
+      return handler.logic(req.params)
+      .then(function handlerRespond(response) {
+        return response;
+      })
+      .catch(function handlerLogicCatch(response) {
+        return response;
+      });
+    }))
+    .then(function respond(responses) {
+      res.send(responses);
+      next();
+    })
+    .catch(function logicCatch(responses) {
+      res.send(responses);
+      next();
+    });
   }
 };
